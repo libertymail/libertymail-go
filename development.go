ad67949d2bfd0d804d32b647a2da0549c5cf122f
RemideZ <remi.de.z@gmail.com>
2013-09-14 23:26:43 +0200

    Added AES functions

--- development.go
+++ development.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
-    //"bytes"
-   // "crypto/aes"
-   // "crypto/cipher"
-	
+	"bytes"
+
     "libertymail-go/bits"
 )
 
 func main() {
     fmt.Println("Hello, world!")
-    fmt.Println(bits.GenerateRandomBlock())
+    key := []byte("example key 1234")
+    plaintext := []byte("exampleplaintext")
+
+	fmt.Println(plaintext)
+    enc, _ := bits.AESEncrypt(plaintext, key)
+    fmt.Println(enc)
+    dec, _ := bits.AESDecrypt(enc, key)
+
+    fmt.Println(dec)
+    fmt.Println(bytes.Equal(plaintext,dec))
 }
+
+
 
